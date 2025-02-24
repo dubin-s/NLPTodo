90008c2e2640b9c1bd33370b633501e41e13d602
Spenser <18bf4e7@proton.me>
2025-02-24 01:20:19 -0500

    Initial Commit

---
+++ NLPTodo/NLPTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPTodoApp.swift
+//  NLPTodo
+//
+//  Created by Spenser Dubin on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NLPTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NLPTodoTests/NLPTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  NLPTodoTests.swift
+//  NLPTodoTests
+//
+//  Created by Spenser Dubin on 2/24/25.
+//
+
+import Testing
+@testable import NLPTodo
+
+struct NLPTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
